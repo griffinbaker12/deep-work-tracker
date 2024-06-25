6c8f1312bb09fc4461ebc43bf09f9e365dac7a91
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2024-06-25 16:45:55 -0500

    remove sites

--- main.py
+++ main.py
@@ -1,26 +1,49 @@
 import argparse
 import subprocess
 
+HOSTS_PATH = "/etc/hosts"
+HEADER_BLOCK = "# Added by work script\n"
+FOOTER_BLOCK = "End of section\n"
 EM_DASH = "\u2014"
 
 
+def already_a_session():
+    with open(HOSTS_PATH, "r") as hosts_file:
+        for line in hosts_file:
+            if line == HEADER_BLOCK:
+                return True
+
+
 def block_sites(sites):
-    header = "# Added by work script\n"
-    entries = []
+    if not already_a_session():
+        entries = []
+        with open(HOSTS_PATH, "a") as hosts_file:
+            for site in sites:
+                entries.extend(
+                    [
+                        f"0.0.0.0 {site}.com",
+                        f"0.0.0.0 www.{site}.com",
+                    ]
+                )
+            hosts_file.write(HEADER_BLOCK)
+            hosts_file.write("\n".join(entries))
+            hosts_file.write(FOOTER_BLOCK)
 
-    with open("/etc/hosts", "a") as hosts_file:
-        for site in sites:
-            entries.extend(
-                [
-                    f"0.0.0.0 {site}.com",
-                    f"0.0.0.0 www.{site}.com",
-                ]
-            )
-        hosts_file.write(header)
-        hosts_file.write("\n".join(entries))
-        hosts_file.write("\n")
+        subprocess.run(["sudo", "killall", "-HUP", "mDNSResponder"])
+    else:
+        print("Error: Already a current study session in progress.")
+        exit(1)
 
-    subprocess.run(["sudo", "killall", "-HUP", "mDNSResponder"])
+
+def remove_sites():
+    with open(HOSTS_PATH, "r") as hosts_file:
+        lines = hosts_file.readlines()
+
+    with open(HOSTS_PATH, "w") as hosts_file:
+        for line in lines:
+            if line == HEADER_BLOCK:
+                break
+            hosts_file.write(line)
 
 
 def main():
@@ -46,6 +69,9 @@ def main():
     )
 
     block_sites(sites)
+
+    # TODO: when the time is up, the remove the sites
+    remove_sites()
 
 
 if __name__ == "__main__":
